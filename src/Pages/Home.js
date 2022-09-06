5c02cdd6684161a34995983b32d2c632b5de5ef6
Shakib Mia <smdshakibmia2001@gmail.com>
2022-09-06 22:00:11 +0600

    join as an investor completed

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../components/Header";
+import JoinAsInvestor from "../components/JoinAsInvestor";
 import WhyChooseUs from "../components/WhyChooseUs";
 const Home = () => {
   return (
     <div>
       <Header />
       <WhyChooseUs />
+      <JoinAsInvestor />
     </div>
   );
 };
---
+++ src/components/JoinAsInvestor.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const JoinAsInvestor = () => {
+  return (
+    <div className="container bg-secondary-blue text-blue text-center py-5 rounded-4">
+      <div className="col-5 mx-auto px-1">
+        <h1>Join Us As An Investor</h1>
+        <p className="my-3">
+          We offer end-to-end business consulting services to help you in all
+          stages of your company's growth.
+        </p>
+        <a href="#" className="btn-blue px-4">
+          Get Started
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default JoinAsInvestor;
