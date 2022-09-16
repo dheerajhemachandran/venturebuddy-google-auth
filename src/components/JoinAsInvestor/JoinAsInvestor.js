371fbb5477ebc5cd9cb9118e49893a8056550e14
Shakib Mia <smdshakibmia2001@gmail.com>
2022-09-16 22:02:06 +0600

    join section updated

--- src/components/JoinAsInvestor/JoinAsInvestor.js
+++ src/components/JoinAsInvestor/JoinAsInvestor.js
@@ -6,13 +6,13 @@ const JoinAsInvestor = () => {
     <div className="container-fluid px-lg-5 py-5">
       <div className="bg-secondary-blue text-blue text-center rounded-4 mx-lg-5">
         <div className="col-12 col-lg-6 mx-auto px-1" id={styles.container}>
-          <h1 className={styles.joinHeader}>Join us as an investor</h1>
+          <h1 className={styles.joinHeader}>Join us as an Investor</h1>
           <p className={`mt-3 mb-5 ${styles.joinPara} col-11 mx-auto`}>
             We offer end-to-end business consulting services to help you in all
             stages of your company's growth.
           </p>
 
-          <a href="#" className="btn-blue" id={styles.joinBtn}>
+          <a href="#" className="btn-blue">
             Get Started
           </a>
         </div>
--- src/components/WhyChooseUs/WhyChooseCard.js
+++ src/components/WhyChooseUs/WhyChooseCard.js
@@ -5,7 +5,7 @@ const WhyChooseCard = ({ img, header, desc }) => {
   return (
     <div className="col" style={styles.whyChooseCard}>
       <div className="col-10 mx-auto">
-        <img src={img} alt="" className="img-fluid" />
+        <img src={img} alt="" className={styles.whyChooseImg} />
         <h2 className={`${styles.whyChooseCardHeader} mx-auto`}>{header}</h2>
         <p className={`col-9 mx-auto ${styles.whyChooseCardParagraph}`}>
           {desc}
